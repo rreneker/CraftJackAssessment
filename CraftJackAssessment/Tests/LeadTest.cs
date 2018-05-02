e14d618cd3fdebcdfde259373c54a0c476a46da6
rreneker <rreneker@umich.edu>
2018-05-02 14:23:07 -0400

    Projects Added
    Class Library and Test Library projects added

---
+++ CraftJackAssessment/Tests/LeadTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class LeadTest
+    {
+        [TestMethod]
+        public void CreateLead()
+        {
+        }
+    }
+}
