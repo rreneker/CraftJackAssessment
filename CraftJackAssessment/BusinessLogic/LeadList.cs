d2ec3ba9f33d4e454ed564e2a5423af5a254ea1d
rreneker <rreneker@umich.edu>
2018-05-02 14:37:08 -0400

    LeadList Class Created

---
+++ CraftJackAssessment/BusinessLogic/LeadList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class LeadList
+    {
+        private List<Lead> Leads;
+
+        public LeadList()
+        {
+            Leads = new List<Lead>();
+        }
+
+        public List<Lead> GetLeads()
+        {
+            return Leads;
+        }
+    }
+}
---
+++ CraftJackAssessment/Tests/LeadListTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BusinessLogic;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    [TestClass]
+    public class LeadListTests
+    {
+        [TestMethod]
+        public void CreateLeadList()
+        {
+            LeadList leads = new LeadList();
+
+            CollectionAssert.AreEqual(new List<Lead>(), leads.GetLeads());
+        }
+    }
+}
