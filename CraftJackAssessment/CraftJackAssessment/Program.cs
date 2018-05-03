0d1d66d1c98b1b3b94a22b1914a6afdbabc0b613
rreneker <rreneker@umich.edu>
2018-05-03 19:06:44 -0400

    Main function functionality added

--- CraftJackAssessment/CraftJackAssessment/Program.cs
+++ CraftJackAssessment/CraftJackAssessment/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogic;
 
 namespace CraftJackAssessment
 {
@@ -10,6 +12,44 @@ namespace CraftJackAssessment
     {
         static void Main(string[] args)
         {
+            LeadList leads = new LeadList();
+
+            try
+            {
+
+                foreach (string line in File.ReadLines(args[0]))
+                {
+                    leads.AddLead(line);
+                }
+
+                Console.WriteLine("");
+                Console.WriteLine("Sorted by Property Type Then Project:");
+                foreach (Lead l in leads.SortByPropertyTypeThenProject())
+                {
+                    Console.WriteLine(l.StringToPrint());
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Sorted by Start Date:");
+                foreach (Lead l in leads.SortByStartDate())
+                {
+                    Console.WriteLine(l.StringToPrint());
+                }
+                Console.WriteLine("");
+                Console.WriteLine("Sorted by Last Name Descending:");
+                foreach (Lead l in leads.SortByLastNameDescending())
+                {
+                    Console.WriteLine(l.StringToPrint());
+                }
+
+                Console.ReadLine();
+
+            }
+            catch
+            {
+                Console.WriteLine("Please Specify the file in the command line");
+            }
+            
+            
         }
     }
 }
